1e2bd5f07133b729944deebb25543076cd99c758
wq316476951 <316476051@qq.com>
2020-07-05 22:46:53 +0800

    无障碍：获取焦点的View修改ContentDescription时不播报

--- feature_ui/src/main/java/com/wang/mytest/feature/ui/accessibility2/Accessibility2Activity.java
+++ feature_ui/src/main/java/com/wang/mytest/feature/ui/accessibility2/Accessibility2Activity.java
@@ -2,6 +2,7 @@ package com.wang.mytest.feature.ui.accessibility2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.wang.mytest.apt.annotation.Route;
 import com.wang.mytest.feature.ui.R;
@@ -18,5 +19,10 @@ public class Accessibility2Activity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accessibility2);
+
+        findViewById(R.id.btn_2).setOnClickListener(view -> {
+            ((TextView) view).setText("Hello");
+            view.setContentDescription("Hello");
+        });
     }
 }
